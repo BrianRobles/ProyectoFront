c781de848ad7807ae3f4b4037416c579ad11ce92
Brian Robles <samurelrobles@gmail.com>
2022-05-26 16:29:35 -0400

    added authentication

--- src/layout/PrivateLayout.jsx
+++ src/layout/PrivateLayout.jsx
@@ -3,18 +3,21 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from '../components/PrivateRoute';
 
 const PrivateLayout = () => {
   return (
-    <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
-      <Sidebar />
-      <div className='flex w-full h-full'>
-        <div className='w-full h-full overflow-y-scroll'>
-          <Outlet />
+    <PrivateRoute>
+      <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
+        <Sidebar />
+        <div className='flex w-full h-full'>
+          <div className='w-full h-full overflow-y-scroll'>
+            <Outlet />
+          </div>
         </div>
+        <ToastContainer hideProgressBar autoClose={3000} />
       </div>
-      <ToastContainer hideProgressBar autoClose={3000}/>
-    </div>
+    </PrivateRoute>
   );
 };
 
